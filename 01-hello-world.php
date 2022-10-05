7dfb5c4c2d6bf6eebdc47855a3efab783ff00391
Slashyo <tom.bik@hotmail.be>
2022-10-05 11:49:13 +0200

    Hello World

---
+++ 01-hello-world.php
@@ -0,0 +1,19 @@
+<?php
+// Commentaire sur une ligne
+
+// On peut mettre des balises PHP n'importe où dans une page HTML
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php // Ouverture de PHP
+    echo "<h1>Hello World</h1>";
+    // Fermeture de PHP ?>
+</body>
+</html>
