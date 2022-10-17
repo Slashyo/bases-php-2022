40d93baf266c109967cfff768e7dd348854ca930
Slashyo <tom.bik@hotmail.be>
2022-10-17 15:49:59 +0200

    calculatrice

--- 14-exe/14-calcul-Tom.php
+++ 14-exe/14-calcul-Tom.php
@@ -7,12 +7,7 @@
     <title>Calculatrice</title>
 </head>
 <body>
-    <h1>Calculatrice</h1>
-    <h2>Exercice calculatrice</h2>
-    <p>Créez une branche et un nouveau fichier dans 14-exe/14-calcul-{prénom}.php<br>
-Envoyez-moi le git pull request sur notre dépôt de la classe</p>
-    <p>$reponse doit contenir la réponse au calcul, ou l'erreur de calcul ou de fonctionnement</p>
-    <form action="" name="calcul" method="POST">
+<form action="" name="calcul" method="POST">
         <input type="text" name="first" />
         <select name="choix">
             <option value="addition">+</option>
@@ -39,40 +34,40 @@ if(isset($_POST['first'],$_POST['choix'],$_POST['second'],)){
     $nombre1 =($_POST['first']);
     $choix = ($_POST['choix']);
     $nombre2 = ($_POST['second']);
-    if($nombre1 != NULL AND $nombre2 != NULL) // Puis on vérifie leur valeur ...
+    if($nombre1 != NULL AND $nombre2 != NULL)
     {
         if($choix == 'division' AND $nombre2 == 0)
         {
-            echo 'On peut pas diviser par 0 voyons !';
+            echo 'On peut pas diviser par 0 !';
         }
         else
         { 
             if($choix == 'addition')
             {
-            $resultat = $nombre1 + $nombre2;
-            echo 'La somme de ces deux nombres est '.$resultat;
+            $reponse = $nombre1 + $nombre2;
+            echo 'La somme de ces nombres est '.$reponse;
             }
             
-            if($choix == 'soustraction') // Si on a choisi la soustraction, on calcul la différence.
+            if($choix == 'soustraction') 
             {
-            $resultat = $nombre1 - $nombre2; // On calcul
-            echo 'La différence de ces deux nombres est '.$resultat; // Puis on affiche le résultat
+            $reponse = $nombre1 - $nombre2; 
+            echo 'La différence de ces nombres est '.$reponse; 
             }
             
             if($choix == 'multiplication')
             { 
-            $resultat = $nombre1 * $nombre2;
-            echo 'La multiplication des deux nombres est '.$resultat;
+            $reponse = $nombre1 * $nombre2;
+            echo 'La multiplication des nombres est '.$reponse;
             }
         
             if($choix == 'division')
             {
-            $resultat = $nombre1 / $nombre2;
-            echo 'Le résultat est '.$resultat;
+            $reponse = $nombre1 / $nombre2;
+            echo 'Le résultat est '.$reponse;
             }
         }
         }
-    else // Si les champs n'ont pas étaient renseigné, on affiche un message d'erreur ...
+    else 
     {
     echo 'Veuillez renseigner tous les champs.';
     }
